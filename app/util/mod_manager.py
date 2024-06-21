09ac5e04fec33c1a33ffaef3ddffb276deba34ff
e1ectronic <2558985693@qq.com>
2024-06-21 15:17:34 +0800

    AIYOU v0.1.2 http请求改为无代理模式

--- app/util/mod_manager.py
+++ app/util/mod_manager.py
@@ -2,6 +2,16 @@ import requests
 
 
 def load_description(path):
-    x = requests.get(path)
-    json_result = x.json()
-    return json_result
+    try:
+        proxies = {
+            "http": None,
+            "https": None,
+        }
+        x = requests.get(path, proxies=proxies)
+        x.raise_for_status()
+        json_result = x.json()
+        return json_result
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
