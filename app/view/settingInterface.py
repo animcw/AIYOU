a67f79897c73489859fc0e4673b5cf213c5cd091
e1ectronic <2558985693@qq.com>
2024-06-22 01:57:18 +0800

    AIYOU v0.1.2.1 添加检查更新及提交反馈功能

--- app/view/settingInterface.py
+++ app/view/settingInterface.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 
 from PyQt5.QtCore import QUrl
@@ -8,21 +7,8 @@ from qfluentwidgets import MessageBox
 
 from app.resource.Pages.setting import Ui_settingWindow
 from app.util.UI_general_method import *
-from app.util.config_modify import config_manager
-
-
-def clean_cache(self, path):
-
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-            show_info_bar(self, 'success', '删除成功！', '＼(＾O＾)／')
-        except Exception as e:
-            show_info_bar(self, 'error', f'删除 {file_path} 失败！', f'{e}')
+from app.util.config_modify import check_update, resource_path, read_version_from_file
+from app.util.get_path import get_data_path, get_cache_path
 
 
 class settingPageInterface(QWidget, Ui_settingWindow):
@@ -31,34 +17,50 @@ class settingPageInterface(QWidget, Ui_settingWindow):
         super().__init__(parent=parent)
         self.setupUi(self)
 
-        self.dataFolder = config_manager.get('programSetting', 'data_dir')
-        self.cacheFolder = config_manager.get('programSetting', 'cache_dir')
+        self.dataFolder = get_data_path()
+        self.cacheFolder = get_cache_path()
+        file_path = resource_path('AppData/version')
+        self.version = read_version_from_file(file_path)
+
         self.programFolderInfo.setText(self.dataFolder)
         self.cacheFolderInfo.setText(self.cacheFolder)
+        self.aboutInfo.setText(self.version)
 
         self.openFolderButton.clicked.connect(self.open_data_folder)
         self.opencacheFolderButton.clicked.connect(self.open_cache_folder)
         self.cleancacheFolderButton.clicked.connect(self.clean_cache_flyout)
 
-        self.pushIssuesButton.clicked.connect(self.in_build_info)
-        self.checkUpdateButton.clicked.connect(self.in_build_info)
+        self.pushIssuesButton.clicked.connect(self.open_github_issues)
+        self.checkUpdateButton.clicked.connect(self.check_update)
 
-    def in_build_info(self):
+    def check_update(self):
+        check_update(self, True)
 
-        show_info_bar(self, 'warning', '正在建设！', '(；・∀・)')
+    def open_github_issues(self):
+        github_issues_url = 'https://github.com/LittleBlackOfKUN/AIYOU/issues'
+        QDesktopServices.openUrl(QUrl(github_issues_url))
 
     def open_data_folder(self):
-
         QDesktopServices.openUrl(QUrl.fromLocalFile(self.dataFolder))
 
     def open_cache_folder(self):
-
         QDesktopServices.openUrl(QUrl.fromLocalFile(self.cacheFolder))
 
     def clean_cache_flyout(self):
-
         title = '你想好要删除缓存了吗？'
         content = """删除缓存后下次转换需要重新下载游戏客户端文件。"""
         w = MessageBox(title, content, self)
         if w.exec():
-            clean_cache(self, self.cacheFolder)
+            self.clean_cache(self.cacheFolder)
+
+    def clean_cache(self, path):
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                show_info_bar(self, 'success', '删除成功！', '＼(＾O＾)／')
+            except Exception as e:
+                show_info_bar(self, 'error', f'删除 {file_path} 失败！', f'{e}')
